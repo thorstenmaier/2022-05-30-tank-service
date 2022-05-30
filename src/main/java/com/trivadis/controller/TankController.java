d977cfcba63059a94360c433e578ff462284d325
Thorsten Maier <Thorsten.Maier@trivadis.com>
2022-05-30 14:50:39 +0200

    Get tank by id

--- src/main/java/com/trivadis/controller/TankController.java
+++ src/main/java/com/trivadis/controller/TankController.java
@@ -1,6 +1,8 @@
 package com.trivadis.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trivadis.dto.TankDto;
@@ -23,15 +25,20 @@ public class TankController {
 		this.tankLevelRepository = tankLevelRepository;
 	}
 
-	@GetMapping("/")
-	public TankDto getTankDetails() {
-		long tankId = 1l;
+	@GetMapping("/tanks/{id}")
+	public TankDto getTankDetails(@PathVariable("id") long tankId) {
 		TankDto tankDto = new TankDto();
 		tankDto.setTankId(tankId);
 		tankDto.setLevel(tankLevelRepository.getLevel(tankId));
 		tankDto.setCapacity(tankCapacityRepository.getCapacity(tankId));
 		tankDto.setRelativeLevel(relativeTankLevelService.getRelativeTankLevel(tankId));
 		return tankDto;
+	}
+
+	// http://localhost:8081/tanks?id=1
+	@GetMapping("/tanks")
+	public TankDto getTankDetailsParam(@RequestParam("id") long tankId) {
+		return getTankDetails(tankId);
 	}
 
 }
